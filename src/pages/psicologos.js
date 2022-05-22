253e59854c6c9749c8eadf96e9adf0f0c4319eba
santiagoep <santiagoemp01@gmail.com>
2022-05-21 23:35:31 -0500

    Adding loader to psychologist list

--- src/pages/psicologos.js
+++ src/pages/psicologos.js
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import usePsychologists from 'hooks/usePsychologists';
 
 import LayoutWithNavbar from 'components/@layouts/LayoutWithNavbar';
@@ -28,6 +29,7 @@ const columns = [
 function SpecialistsPage() {
   const {
     data: { psychologists },
+    isLoading,
   } = usePsychologists();
 
   const renderField = (psychologist, key) => {
@@ -50,26 +52,43 @@ function SpecialistsPage() {
   return (
     <main className="bg-transparent h-full overflow-y-auto">
       <h1 className="text-center text-3xl font-bold mt-5">Psicólogos</h1>
-      <p className="text-center text-secondary-300 font-light text-sm md:text-base">Datos de contacto de psicólogos en Colombia</p>
+      <p className="text-center text-secondary-300 font-light text-sm md:text-base">
+        Datos de contacto de psicólogos en Colombia
+      </p>
       <ul className="grid grid-cols-1 lg:grid-cols-2 gap-10 px-4 md:px-10 mt-10 pb-5 overflow-y-auto">
-        {psychologists.map((psychologist, psychologistIndex) => (
-          <li
-            ley={`${psychologist.name}-${psychologistIndex}`}
-            className="shadow-lg bg-general-bg rounded-lg"
-          >
-            <ul className="pb-4 grid grid-flow-row gap-2">
-              <p className="rounded-t-lg text-center py-4 px-2 border-b text-sm md:text-base font-bold bg-secondary-100/20">{psychologist.name}</p>
-              {columns.map(({ key, value }) => psychologist[key] && (
-                <li className="grid grid-cols-3 gap-4 text-sm font-light px-4">
-                  <span className="font-bold ml-1">{value}</span>
-                  <div className="col-span-2">
-                    {renderField(psychologist, key)}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+        {(isLoading ? Array(6).fill({}) : psychologists).map(
+          (psychologist, psychologistIndex) => (
+            <li
+              ley={`${psychologist.name}-${psychologistIndex}`}
+              className="shadow-lg bg-general-bg rounded-lg"
+            >
+              <ul className="pb-4 grid grid-flow-row gap-2">
+                <div className="rounded-t-lg text-center py-4 px-2 border-b bg-secondary-100/20">
+                  <p className={cn('text-sm md:text-base font-bold', {
+                    'animate-pulse bg-secondary-100 rounded-lg w-3/5 h-5 block mx-auto': isLoading,
+                  })}
+                  >
+                    {psychologist.name}
+                  </p>
+                </div>
+                {columns.map(
+                  ({ key, value }) => (
+                    <li className="grid grid-cols-3 gap-4 text-sm font-light px-4">
+                      <span className="font-bold ml-1">{value}</span>
+                      <div
+                        className={cn('col-span-2', {
+                          'animate-pulse bg-secondary-100 rounded-lg w-full h-5 block': isLoading,
+                        })}
+                      >
+                        {renderField(psychologist, key)}
+                      </div>
+                    </li>
+                  ),
+                )}
+              </ul>
+            </li>
+          ),
+        )}
       </ul>
     </main>
   );
